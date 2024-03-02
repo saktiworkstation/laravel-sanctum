0bd57a6487fbfbd7f75d5e2bdd29288753473235
saktiworkstation <saktiworkstation@gmail.com>
2024-03-02 13:58:23 +0700

    Make authcontroller and register method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $datauser = new User();
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'proses validasai gagal',
+                'data' => $validator->errors(),
+            ]);
+        }
+    }
+}
